3649c945e028ddb436559af5f700275692ac1c9d
Alocias <alocia0@gmail.com>
2018-06-02 16:34:11 +0200

    extended plot scipt

--- source/scripts/plotOneBodyDensities.py
+++ source/scripts/plotOneBodyDensities.py
@@ -2,9 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
-data = np.loadtxt(sys.argv[1])
+fname = sys.argv[1]
 
-r = np.linspace(0,3,500)
+data = np.loadtxt(fname)
+
+rmaxIdx = fname.index("x")
+rmax = float(fname[rmaxIdx+1:rmaxIdx+4])
+
+r = np.linspace(0,rmax,600)
 
 plt.plot(r, data, 'o', markersize=0.5)
 plt.show()
